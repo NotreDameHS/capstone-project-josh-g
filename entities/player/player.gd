7b82542b3ca101974390ce68b770620ff1da1130
brunson-burner <babylakeshow@gmail.com>
2026-05-26 10:34:58 -0600

    Added new textures, added smooth player movement

---
+++ entities/player/player.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+var velocity := Vector2(0, 0)
+var normal_speed := 600.0
+var boost_speed := 1500.0
+var max_speed := normal_speed
+var health := 10
+var score := 0
+
+
+# Called when the node enters the scene tree for the first time.
+func set_health(new_health: int) -> void:
+	health = new_health
+	get_node("UI/HealthBar").value = health
+	
+	
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	set_health(health)
+	
+	 # Replace with function body.
+
+var steering_factor := 10.0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var direction := Vector2(0, 0)
+	
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+		
+	var desired_velocity := direction * max_speed
+	var steering_vector := desired_velocity - velocity
+	
+	velocity += steering_vector * steering_factor * delta
+	position += velocity * delta
+	
+	if direction.length() > 0.0:
+		get_node("Sprite2D").rotation = velocity.angle()
+	
+	if velocity.length() > 0.0:
+		get_node("Sprite2D").rotation = velocity.angle()
+		
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+
+func _on_area_entered(area: Area2D) -> void:
+		
+	if area.is_in_group("healing_item"):
+		set_health(health + 10)
+	
+
+
+
+
+func _on_timer_timeout() -> void:
+	pass
---
+++ entities/spawn/random_collectible_spawn.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var item_scenes := [
+	preload("res://entities/collectibles/gold_star/gold_star.tscn"),
+	preload("res://entities/collectibles/health_pack/health_pack.tscn")
+]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass	
+
+
+	
+	
+	
+	
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	var random_item_scene: PackedScene = item_scenes.pick_random()
+	var item_instance := random_item_scene.instantiate()
+	add_child(item_instance)
+	
+	var viewport_size := get_viewport_rect().size
+	
+	var random_position := Vector2(0.0, 0.0)
+	random_position.x = randf_range(0.0, viewport_size.x)
+	random_position.y = randf_range(0.0, viewport_size.y)
+	
+	item_instance.position = random_position
